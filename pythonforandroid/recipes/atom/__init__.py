4e40730c876ca9a7f0e1d959725c63e2ec7ac2b7
RobertF <34464649+RobertFlatt@users.noreply.github.com>
2025-03-11 13:52:12 -1000

    Update __init__.py
    Update version, add cppy dependency.

--- pythonforandroid/recipes/atom/__init__.py
+++ pythonforandroid/recipes/atom/__init__.py
@@ -3,8 +3,9 @@ from pythonforandroid.recipe import CppCompiledComponentsPythonRecipe
 
 class AtomRecipe(CppCompiledComponentsPythonRecipe):
     site_packages_name = 'atom'
-    version = '0.3.10'
-    url = 'https://github.com/nucleic/atom/archive/master.zip'
+    version = '0.11.0'
+    url = 'https://github.com/nucleic/atom/archive/refs/tags/{version}.zip'
+    hostpython_prerequisites = ['cppy']
     depends = ['setuptools']
 
 
